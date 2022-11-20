872443dc6a584d4933947bc925e0f37e2bd529ea
Valentina Garavaglia <vaahxx@gmail.com>
2022-11-20 16:27:03 -0500

    styling sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Diego Fernandes" content="Lorem ipsum dolor sit amet" />
-      <Post author="Valentina Garavaglia" content="New Post" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego Fernandes" content="Lorem ipsum dolor sit amet" />
+          <Post author="Valentina Garavaglia" content="New Post" />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <strong>Valentina Garavaglia</strong>
+        <span>Web developer</span>
+      </div>
+      <footer>
+        <a href="#">Edit your profile</a>
+      </footer>
+    </aside>
+  );
+}
